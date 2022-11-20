ab0aac988913398f02dbf9eca3f74256e2b54c20
Sayantan majee <sayantan.majee24@gmail.com>
2022-11-21 02:06:34 +0530

    commiadasd

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ if (PORT == null || PORT === "") {
     PORT = 8000;
 }
 
+Connection()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
@@ -18,6 +20,6 @@ app.use('/',routes);
 
 
 
-Connection();
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
